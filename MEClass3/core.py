3772230bebeec44302fb9da92711b1377cd75efa
manojwustl <manoj@wustl.edu>
2022-03-01 14:30:50 -0600

    proc reg

--- MEClass3/core.py
+++ MEClass3/core.py
@@ -386,77 +386,6 @@ def exec_filter(args):
     df_expr.to_csv(output_expr_file_name, sep='\t', na_rep='NA', float_format='%.3f') #, header=None)
 
 #---------------------------------------------------------------------------
-def exec_proc_reg(args):
-
-    # This part of code consolidates regions from different cell-type predictions.
-    # Input is standard .bedgraph
-    #
-    gap_join = args.gtj_inp #500    
-    max_size = args.msz_inp #10000
-    path_to_output = os.getcwd()
-    if not os.path.isdir('enhc_bed'):
-        os.makedirs('enhc_bed')    
-#    path_to_output = args.pto_inp
-    #
-    enhc_fofn = args.efn_inp
-    with open(enhc_fofn, 'r') as enhc_fofn_file:
-        enhc_fofn_file_lines = enhc_fofn_file.readlines()
-    enhc_fofn_file.close()
-    #
-    # Assign file names to a dictionary
-    df_type_name = {}
-    for item in enhc_fofn_file_lines:
-        if item.startswith('#'):
-            continue
-        df_type_name[item.strip().split()[0]] = pd.read_table(item.strip().split()[1], \
-                 compression='gzip', names = ['chrom', 'chromStart', 'chromEnd', \
-                'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRgb', \
-                'blockCount', 'blockSizes', 'blockStarts']) 
-    #
-    df_chrom = {}
-    df_chrom_cons = {}
-    chr_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', \
-                    '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
-    for i in chr_list:
-        chrom = 'chr'+i
-
-        df_chrom[chrom] = pd.DataFrame(columns = ['chrom', 'chromStart', 'chromEnd', 'name', 'score', \
-            'strand', 'thickStart', 'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts'])
-
-        df_chrom[chrom][['chromStart', 'chromEnd', 'thickStart', 'thickEnd']] = \
-            df_chrom[chrom][['chromStart', 'chromEnd', 'thickStart', 'thickEnd']].astype(int) # change datatype to int
-        
-        # extract chromosome specific regions
-        for item in enhc_fofn_file_lines:
-            if item.startswith('#'):
-                continue
-            df_chrom[chrom] = df_chrom[chrom].append( \
-                    df_type_name[item.strip().split()[0]].loc[ df_type_name[item.strip().split()[0]].chrom == chrom, : ] )
-
-        # Short, reset index, reemove duplicates and print bed file
-        df_chrom[chrom] = df_chrom[chrom].sort_values('thickStart')
-        df_chrom[chrom] = df_chrom[chrom].reset_index(drop=True)
-        df_chrom[chrom] = df_chrom[chrom].drop_duplicates(subset=['thickStart', 'thickEnd'])
-        df_chrom[chrom].to_csv(path_to_output+'/enhc_bed/'+chrom+'_all_cell_type.bed', sep='\t', index=False)
-
-        #Condolidate regions
-        df_chrom_cons[chrom] = pd.DataFrame(columns = ['chrom', 'thickStart', 'thickEnd', 'length'])
-        df_chrom_cons[chrom][['thickStart', 'thickEnd', 'length']] = \
-                df_chrom_cons[chrom][['thickStart', 'thickEnd', 'length']].astype(int)
-        prev_start = df_chrom[chrom].loc[0, 'thickStart']
-        prev_end = df_chrom[chrom].loc[0, 'thickEnd']
-        for item in df_chrom[chrom].index:
-            if df_chrom[chrom].loc[item, 'thickStart'] <= prev_end + gap_join:
-                if df_chrom[chrom].loc[item, 'thickEnd'] > prev_end:
-                    prev_end = df_chrom[chrom].loc[item, 'thickEnd']
-            if df_chrom[chrom].loc[item, 'thickStart'] > prev_end + gap_join:
-                if prev_end-prev_start <= max_size:
-                    df_chrom_cons[chrom].loc[item] = [chrom, prev_start, prev_end, prev_end-prev_start]
-                prev_start = df_chrom[chrom].loc[item, 'thickStart']
-                prev_end = df_chrom[chrom].loc[item, 'thickEnd']
-
-        df_chrom_cons[chrom].to_csv(path_to_output+'/enhc_bed/'+chrom+'_all_cell_type_cons.bed', sep='\t', index=False, header=None)
-#---------------------------------------------------------------------------
 def exec_proximity_list(args):
 
     # This part of code generate proximity list of regulatory elements
@@ -1190,7 +1119,7 @@ def exec_run_clf(args):
 #---------------------------------------------------------------------------
 def exec_eval(args):
 
-    df = pd.read_csv( args.dfi_inp, index_col=[0] ).reset_index(drop=True)
+    df = pd.read_csv( args.dfi_inp, index_col=[0] ).reset_index()
     steps = args.steps_inp
     #
     output_file_name = ((args.dfi_inp).strip('.csv'))+'_acc_rej.txt'
@@ -1200,7 +1129,7 @@ def exec_eval(args):
     totalGenes_P = 0
     totalGenes_N = 0
     #
-    totalGenes = df['gene_id'].count()
+    totalGenes = df['gene_id-sample_name'].count()
     #
     expr_flag_value_count = df['expr_flag'].value_counts()
     try:
@@ -1230,14 +1159,14 @@ def exec_eval(args):
         #
         #
         df_tmp_P = df_tmp[ (df_tmp['prob_up'] >= 1-threshold) ]
-        numGenes_P = df_tmp_P['gene_id'].count()
+        numGenes_P = df_tmp_P['gene_id-sample_name'].count()
         df_tmp_P = df_tmp_P[ (df_tmp_P['expr_flag'] == 1) & (df_tmp_P['expr_pred'] == 1) ]
-        TP_P = df_tmp_P['gene_id'].count()
+        TP_P = df_tmp_P['gene_id-sample_name'].count()
         #
         df_tmp_N = df_tmp[ (df_tmp['prob_dw'] >= 1-threshold) ]
-        numGenes_N = df_tmp_N['gene_id'].count()
+        numGenes_N = df_tmp_N['gene_id-sample_name'].count()
         df_tmp_N = df_tmp_N[ ( df_tmp_N['expr_flag'] == -1 ) & (df_tmp_N['expr_pred'] == -1) ]
-        TP_N = df_tmp_N['gene_id'].count()
+        TP_N = df_tmp_N['gene_id-sample_name'].count()
         #sys.stdout.write( str(threshold)+'\t'+str(numGenes_P)+'\t'+str(TP_P)+'\t'+str(numGenes_N)+'\t'+str(TP_N)+'\n')
         #
         df_tmp = df_tmp.iloc[0:0]
@@ -1278,18 +1207,167 @@ def exec_eval(args):
     sys.stdout.write( '#Total_Genes:\t'+str( totalGenes )+'\n' )
 
 #---------------------------------------------------------------------------
+def exec_proc_reg(args):
+
+    gap_join = args.gapj_inp # 500
+    max_size = args.max_inp # 10000
+    tss_win = args.twin_inp # 2000
+    input_gene_file = args.glf_inp
+    #
+    path_to_output = os.getcwd()
+    if not os.path.isdir('enhc_bed'):
+        os.makedirs('enhc_bed')
+    #
+    # Store TSS information.
+    with open(input_gene_file, 'r') as refgene_file:
+        refgene_file_lines = refgene_file.readlines()
+    refgene_file.close()
+
+    dict_refgene = {}
+    for line in refgene_file_lines:
+        refgene_items = line.strip().split()
+        #
+        if not refgene_items[1].startswith('NM_'):
+            continue
+        #
+        chrom, strand, tx_start, tx_end = \
+            refgene_items[2], refgene_items[3], int(refgene_items[4]), int(refgene_items[5])
+        tss = tx_start if strand == '+' else tx_end
+        #
+        try:
+            dict_refgene[chrom].append(tss-tss_win)
+        except KeyError:
+            dict_refgene[chrom] = []
+            dict_refgene[chrom].append(tss-tss_win)
+        #
+    for chrom_name, chrom_items in dict_refgene.items():
+        #
+        dict_refgene[chrom_name] = sorted( list(set(dict_refgene[chrom_name])) , key=int )
+    #
+    #print(len(list(set(dict_refgene['chr1']))))
+    #sys.exit()
+    #
+    enhc_fofn = 'enhc_list' #args.efn_inp
+    with open(enhc_fofn, 'r') as enhc_fofn_file:
+        enhc_fofn_file_lines = enhc_fofn_file.readlines()
+    enhc_fofn_file.close()
+    #
+    # Assign file names to a dictionary
+    df_type_name = {}
+    for item in enhc_fofn_file_lines:
+        if item.startswith('#'):
+            continue
+        df_type_name[item.strip().split()[0]] = pd.read_table(item.strip().split()[1], \
+                 compression='gzip', names = ['chrom', 'chromStart', 'chromEnd', \
+                'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRgb', \
+                'blockCount', 'blockSizes', 'blockStarts'])
+        df_type_name[item.strip().split()[0]]['ctp'] = item.strip().split()[0]
+    #
+    df_chrom = {}
+    df_ctp_chrom = {}
+    df_chrom_cons = {}
+    df_chrom_notss = {}
+    #
+    chr_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', \
+                    '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
+    for i in chr_list:
+        chrom = 'chr'+i
+
+        df_chrom[chrom] = pd.DataFrame(columns = ['chrom', 'chromStart', 'chromEnd', 'name', 'score', \
+            'strand', 'thickStart', 'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts', 'ctp'])
+
+        df_chrom[chrom][['chromStart', 'chromEnd', 'thickStart', 'thickEnd']] = \
+            df_chrom[chrom][['chromStart', 'chromEnd', 'thickStart', 'thickEnd']].astype(int) # change datatype to int
+
+        # extract chromosome specific regions
+        for item in enhc_fofn_file_lines:
+            if item.startswith('#'):
+                continue
+            df_chrom[chrom] = df_chrom[chrom].append( \
+                    df_type_name[item.strip().split()[0]].loc[ df_type_name[item.strip().split()[0]].chrom == chrom, : ] )
+
+        # Short, reset index, reemove duplicates and print bed file
+        df_chrom[chrom] = df_chrom[chrom].sort_values('thickStart')
+        df_chrom[chrom] = df_chrom[chrom].reset_index(drop=True)
+        df_ctp_chrom[chrom] = df_chrom[chrom].groupby(['chrom', 'thickStart', 'thickEnd'])['ctp'].apply(list).to_frame().reset_index()#.rename(columns={0:'count'})
+        df_chrom[chrom] = df_chrom[chrom].groupby(['chrom', 'thickStart', 'thickEnd']).size().reset_index().rename(columns={0:'count'})
+        df_chrom[chrom] = df_chrom[chrom].merge(df_ctp_chrom[chrom])
+        df_chrom[chrom]['ctp'] = df_chrom[chrom]['ctp'].apply(lambda x: ','.join(x))
+        df_chrom[chrom]['count'] = df_chrom[chrom]['count'].astype(int)
+        df_chrom[chrom].to_csv(path_to_output+'/enhc_bed/'+chrom+'_all_cell_type.bed', sep='\t', index=False)
+        #
+        # Remove those falling in TSS region
+        df_chrom_notss[chrom] = pd.DataFrame(columns = ['chrom', 'thickStart', 'thickEnd', 'count', 'ctp'])
+        df_chrom_notss[chrom][['thickStart', 'thickEnd', 'count']] = \
+                df_chrom_notss[chrom][['thickStart', 'thickEnd', 'count']].astype(int)
+        #
+        write_flag = 1
+        for item in df_chrom[chrom].index:
+            #
+            if df_chrom[chrom].loc[item, 'thickStart'] > df_chrom[chrom].loc[item, 'thickEnd']:
+                sys.stdout.write('thickStart > thickEnd \n')
+                sys.exit()
+            #
+            for tss_item in dict_refgene[chrom]: #
+                #
+                if df_chrom[chrom].loc[item, 'thickStart'] > tss_item+(2*tss_win):
+                    continue
+                #
+                if ( tss_item <= df_chrom[chrom].loc[item, 'thickStart'] <= tss_item+(2*tss_win) ) or \
+                    ( tss_item <= df_chrom[chrom].loc[item, 'thickEnd'] <= tss_item+(2*tss_win) ) or \
+                    ( df_chrom[chrom].loc[item, 'thickStart'] <= tss_item <=  df_chrom[chrom].loc[item, 'thickEnd']):
+                    write_flag = 0
+                    break
+                #
+                if tss_item > df_chrom[chrom].loc[item, 'thickEnd']:
+                    break
+            #
+            if write_flag == 1:
+                df_chrom_notss[chrom].loc[item] = df_chrom[chrom].loc[item, :]
+            #
+            write_flag = 1
+        #
+        df_chrom_notss[chrom] = df_chrom_notss[chrom].reset_index(drop=True)
+        df_chrom_notss[chrom].to_csv(path_to_output+'/enhc_bed/'+chrom+'_all_cell_type_noTSS.bed', sep='\t', index=False)
+
+        #Condolidate overlapping
+        df_chrom_cons[chrom] = pd.DataFrame(columns = ['chrom', 'thickStart', 'thickEnd', 'count', 'ctp'])
+        df_chrom_cons[chrom][['thickStart', 'thickEnd', 'count']] = \
+                df_chrom_cons[chrom][['thickStart', 'thickEnd', 'count']].astype(int)
+        prev_start = df_chrom_notss[chrom].loc[0, 'thickStart']
+        prev_end = df_chrom_notss[chrom].loc[0, 'thickEnd']
+        prev_count = 0 # df_chrom[chrom].loc[0, 'count']
+        prev_ctp = '' #list(df_chrom[chrom].loc[0, 'ctp'])
+        #
+        for item in df_chrom_notss[chrom].index:
+            if df_chrom_notss[chrom].loc[item, 'thickStart'] <= prev_end + gap_join:
+                prev_count = prev_count + df_chrom_notss[chrom].loc[item, 'count']
+                prev_ctp = prev_ctp +','+ df_chrom_notss[chrom].loc[item, 'ctp']
+                if df_chrom_notss[chrom].loc[item, 'thickEnd'] > prev_end:
+                    prev_end = df_chrom_notss[chrom].loc[item, 'thickEnd']
+            if df_chrom_notss[chrom].loc[item, 'thickStart'] > prev_end + gap_join:
+                if prev_end-prev_start <= max_size:
+                    df_chrom_cons[chrom].loc[item] = [chrom, prev_start, prev_end, prev_count, prev_ctp]
+                prev_start = df_chrom_notss[chrom].loc[item, 'thickStart']
+                prev_end = df_chrom_notss[chrom].loc[item, 'thickEnd']
+                prev_count = df_chrom_notss[chrom].loc[item, 'count']
+                prev_ctp = df_chrom_notss[chrom].loc[item, 'ctp']
+
+        df_chrom_cons[chrom].to_csv(path_to_output+'/enhc_bed/'+chrom+'_all_cell_type_noTSS_cons.bed', sep='\t', index=False, header=None)
+
+#---------------------------------------------------------------------------
 
 def main():
     FUNCTION_MAP = {
-            'proc_sample'        : exec_proc_sample,
-            'filter'        : exec_filter,
-            'proc_reg'        : exec_proc_reg,
+            'proc_sample'       : exec_proc_sample,
+            'filter'            : exec_filter,
+            'proc_reg'          : exec_proc_reg,
             'proximity_list'    : exec_proximity_list,
-            'interp'        : exec_interp,
+            'interp'            : exec_interp,
             'merge_features'    : exec_merge_features,
             'setup_expr'        : exec_setup_expr,
-            'run_clf'        : exec_run_clf,
-            'eval'          : exec_eval
+            'run_clf'           : exec_run_clf,
+            'eval'              : exec_eval,
             }
 
     parser = argparse.ArgumentParser()
@@ -1320,11 +1398,6 @@ def main():
     sp_merge_features.add_argument('-nst', action='store', dest='nst_inp', type=int, default=1, help='nend start index')
     sp_merge_features.add_argument('-ned', action='store', dest='ned_inp', type=int, default=2, help='nend end index')
     sp_merge_features.add_argument('-tif', action='store', dest='tif_inp', help='tss interpolation file')
-
-    sp_proc_reg = subparsers.add_parser('proc_reg', help='proc reg argument')
-    sp_proc_reg.add_argument('-gtj', action='store', dest='gtj_inp', type=int, default=500, help='maximum gap to join together')
-    sp_proc_reg.add_argument('-msz', action='store', dest='msz_inp', type=int, default=10000, help='maximum size of re')
-    sp_proc_reg.add_argument('-efn', action='store', dest='efn_inp', help='fofn enhc prediction')
 
     sp_interp = subparsers.add_parser('interp', help='interp argument')
     sp_interp.add_argument('-sigma', action='store', dest='sigma_inp', type=int, default=50, help='Value of sigma')
@@ -1360,6 +1433,12 @@ def main():
     sp_eval = subparsers.add_parser('eval', help='Evaluation argument')
     sp_eval.add_argument('-dfi', action='store', dest='dfi_inp', help='Dataframe output from classification')
     sp_eval.add_argument('-nstp', action='store', dest='steps_inp', type=int, default=101, help='Number of steps')
+
+    sp_proc_reg = subparsers.add_parser('proc_reg', help='Process RE regions')
+    sp_proc_reg.add_argument('-gpj', action='store', dest='gapj_inp', type=int, default=500, help='RE closer than this will be joined together.')
+    sp_proc_reg.add_argument('-msz', action='store', dest='max_inp', type=int, default=1000, help='Maximum size of RE to take into account.')
+    sp_proc_reg.add_argument('-twn', action='store', dest='twin_inp', type=int, default=2000, help='RE falling +/- is this region around TSS will be ignored.')
+    sp_proc_reg.add_argument('-glf', action='store', dest='glf_inp', help='Gene information file')
  
     args = parser.parse_args()
     funct = FUNCTION_MAP[args.command]
