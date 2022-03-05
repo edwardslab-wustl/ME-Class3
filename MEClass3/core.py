990c4a95f7c7f8df19fdf53651d3a5a4f0d58b00
manojwustl <manoj@wustl.edu>
2022-03-05 11:23:48 -0600

    fixes

--- MEClass3/core.py
+++ MEClass3/core.py
@@ -516,7 +516,9 @@ def exec_interp(args):
     num_intrp_point = args.nip_inp
     reg_fofn = args.rfn_inp
     sample_fofn = args.sfn_inp
+    #
     interp_bin = args.ibin_inp
+    #
     anchor_window = args.anch_win
     #
     # Readin region file info
@@ -1122,6 +1124,7 @@ def exec_eval(args):
 
     df = pd.read_csv( args.dfi_inp, index_col=[0] ).reset_index()
     steps = args.steps_inp
+    tag = ((args.dfi_inp).strip().split('.'))[0]
     #
     output_file_name = ((args.dfi_inp).strip('.csv'))+'_acc_rej.txt'
     #
@@ -1141,11 +1144,6 @@ def exec_eval(args):
             totalGenes_N = pd.Series(expr_flag_value_count)[-1]
     except KeyError:
             totalGenes_N = 0
-    #
-    gene_count_90_per_acc = 0
-    gene_count_85_per_acc = 0
-    gene_count_80_per_acc = 0
-    gene_count_75_per_acc = 0
     #
     output_file = open(output_file_name,'w')
     #
@@ -1170,10 +1168,6 @@ def exec_eval(args):
         TP_N = df_tmp_N['gene_id-sample_name'].count()
         #sys.stdout.write( str(threshold)+'\t'+str(numGenes_P)+'\t'+str(TP_P)+'\t'+str(numGenes_N)+'\t'+str(TP_N)+'\n')
         #
-        df_tmp = df_tmp.iloc[0:0]
-        df_tmp_P = df_tmp_P.iloc[0:0]
-        df_tmp_N = df_tmp_N.iloc[0:0]
-        #
         accuracy = 0
         rejectRate = 0
         #
@@ -1184,27 +1178,52 @@ def exec_eval(args):
         #
         if ( accuracy >= 0.90) :
                 gene_count_90_per_acc = ( numGenes_P + numGenes_N )
+                sample_freq_90 = df_tmp['sample_name'].value_counts()
         if ( accuracy >= 0.85) :
                 gene_count_85_per_acc = ( numGenes_P + numGenes_N )
+                sample_freq_85 = df_tmp['sample_name'].value_counts()
         if ( accuracy >= 0.80) :
                 gene_count_80_per_acc = ( numGenes_P + numGenes_N )
+                sample_freq_80 = df_tmp['sample_name'].value_counts()
         if ( accuracy >= 0.75) :
                 gene_count_75_per_acc = ( numGenes_P + numGenes_N )
+                sample_freq_75 = df_tmp['sample_name'].value_counts()
+        #
+        df_tmp = df_tmp.iloc[0:0]
+        df_tmp_P = df_tmp_P.iloc[0:0]
+        df_tmp_N = df_tmp_N.iloc[0:0]
         #
         output_file.write(str(threshold)+'\t'+str(rejectRate)+'\t'+str(accuracy)+'\t'+str( numGenes_P + numGenes_N )+'\n')
 
     output_file.close()
-
+    #
     y_true = df['expr_flag'].tolist()
     y_pred = df['expr_pred'].tolist()
-
+    #
     sys.stdout.write( 'F1 Score (macro):\t'+str( (f1_score(y_true, y_pred, average='macro') ))+'\n' )
     sys.stdout.write( 'F1 Score (micro):\t'+str( (f1_score(y_true, y_pred, average='micro') ))+'\n' )
     sys.stdout.write( 'Accuracy score:\t'+str( accuracy_score(y_true, y_pred) )+'\n' )
+    #
     sys.stdout.write( '#Genes with >90% accuracy:\t'+str( gene_count_90_per_acc )+'\n' )
+    with open( tag+'_genes_with_90_percent_accuracy.txt', 'w') as output_90per:
+        for sample in sample_freq_90.keys():
+            output_90per.write( str(pd.Series( sample_freq_90 )[sample]) +'\t'+ sample +'\n' )
+    #
     sys.stdout.write( '#Genes with >85% accuracy:\t'+str( gene_count_85_per_acc )+'\n' )
+    with open( tag+'_genes_with_85_percent_accuracy.txt', 'w') as output_85per:
+        for sample in sample_freq_85.keys():
+            output_85per.write( str(pd.Series( sample_freq_85 )[sample]) +'\t'+ sample +'\n' )
+    #
     sys.stdout.write( '#Genes with >80% accuracy:\t'+str( gene_count_80_per_acc )+'\n' )
+    with open( tag+'_genes_with_80_percent_accuracy.txt', 'w') as output_80per:
+        for sample in sample_freq_80.keys():
+            output_80per.write( str(pd.Series( sample_freq_80 )[sample]) +'\t'+ sample +'\n' )
+    #
     sys.stdout.write( '#Genes with >75% accuracy:\t'+str( gene_count_75_per_acc )+'\n' )
+    with open( tag+'_genes_with_75_percent_accuracy.txt', 'w') as output_75per:
+        for sample in sample_freq_75.keys():
+            output_75per.write( str(pd.Series( sample_freq_75 )[sample]) +'\t'+ sample +'\n' )
+
     sys.stdout.write( '#Total_Genes:\t'+str( totalGenes )+'\n' )
 
 #---------------------------------------------------------------------------
