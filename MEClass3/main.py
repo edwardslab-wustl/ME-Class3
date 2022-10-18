128f7ba6e70ac2d353cbf98e181bea7c6b1ab2b8
jredwards417 <jredwards@wustl.edu>
2022-10-18 10:59:45 -0500

    rearranged code, removed old subcommands

--- MEClass3/main.py
+++ MEClass3/main.py
@@ -7,15 +7,15 @@ import argparse
 
 from MEClass3.subcommand_parser_functions import setup_subparsers, Subcommand
 from MEClass3.process_sample import exec_proc_sample, exec_proc_sample_help
-from MEClass3.filter import exec_filter, exec_filter_help
-from MEClass3.setup_expr import exec_setup_expr, exec_setup_expr_help
-from MEClass3.merge_features import exec_merge_features, exec_merge_features_help
 from MEClass3.interpolation import exec_interp, exec_interp_help
 from MEClass3.proximity_list import exec_proximity_list, exec_proximity_list_help
-from MEClass3.process_regions import exec_proc_reg, exec_proc_reg_help
 from MEClass3.classifier import exec_run_clf, exec_run_clf_help
 from MEClass3.evaluation import exec_eval, exec_eval_help
 from MEClass3.merge_data import exec_merge_data, exec_merge_data_help
+#from MEClass3.filter import exec_filter, exec_filter_help
+#from MEClass3.setup_expr import exec_setup_expr, exec_setup_expr_help
+#from MEClass3.merge_features import exec_merge_features, exec_merge_features_help
+#from MEClass3.process_regions import exec_proc_reg, exec_proc_reg_help
 
 def main():
     meclass3_description = '''Tool for integrative analysis of methylation and expression data'''
@@ -28,18 +28,6 @@ def main():
 def setup_subcommands():       
     subcommands = []
     
-    subcommands.append( Subcommand( name="filter", 
-                                function=exec_filter,
-                                help_function=exec_filter_help,
-                                help = "Filters data",
-                                desc = "Filter description"))
-
-    subcommands.append( Subcommand( name="interp", 
-                                function=exec_interp,
-                                help_function=exec_interp_help,
-                                help = "interp data",
-                                desc = "interp description"))
-
     subcommands.append( Subcommand( name="proc_sample", 
                                 function=exec_proc_sample,
                                 help_function=exec_proc_sample_help,
@@ -51,18 +39,12 @@ def setup_subcommands():
                                 help_function=exec_proximity_list_help,
                                 help = "Proximity list",
                                 desc = "proximity list description"))
-
-    subcommands.append( Subcommand( name="merge_features", 
-                                function=exec_merge_features,
-                                help_function=exec_merge_features_help,
-                                help = "Merge features",
-                                desc = "Merge features description"))
-
-    subcommands.append( Subcommand( name="setup_expr", 
-                                function=exec_setup_expr,
-                                help_function=exec_setup_expr_help,
-                                help = "setup expr",
-                                desc = "setup expr description"))
+    
+    subcommands.append( Subcommand( name="interp", 
+                                function=exec_interp,
+                                help_function=exec_interp_help,
+                                help = "interp data",
+                                desc = "interp description"))
 
     subcommands.append( Subcommand( name="run_clf", 
                                 function=exec_run_clf,
@@ -82,11 +64,30 @@ def setup_subcommands():
                                 help = "Merge data",
                                 desc = "Merge data description"))
 
-    subcommands.append( Subcommand( name="proc_reg", 
-                                function=exec_proc_reg,
-                                help_function=exec_proc_reg_help,
-                                help = "Process region",
-                                desc = "Process region description"))
+#    subcommands.append( Subcommand( name="merge_features", 
+#                                function=exec_merge_features,
+#                                help_function=exec_merge_features_help,
+#                                help = "Merge features",
+#                                desc = "Merge features description"))
+
+#    subcommands.append( Subcommand( name="setup_expr", 
+#                                function=exec_setup_expr,
+#                                help_function=exec_setup_expr_help,
+#                                help = "setup expr",
+#                                desc = "setup expr description"))
+
+#    subcommands.append( Subcommand( name="proc_reg", 
+#                                function=exec_proc_reg,
+#                                help_function=exec_proc_reg_help,
+#                                help = "Process region",
+#                                desc = "Process region description"))
+
+#    subcommands.append( Subcommand( name="filter", 
+#                                function=exec_filter,
+#                                help_function=exec_filter_help,
+#                                help = "Filters data",
+#                                desc = "Filter description"))
+
 
     return subcommands
 
